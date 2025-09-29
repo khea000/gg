ecbd7e1162c13836cb71ee6eb94b1e34892a6ddb
khea000 <alkheyaffirrdzz@gmail.com>
2025-09-29 11:21:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function hitung(operator) {
+  let angka1 = parseFloat(document.getElementById('angka1').value);
+  let angka2 = parseFloat(document.getElementById('angka2').value);
+  let hasil = 0;
+
+  if (isNaN(angka1) || isNaN(angka2)) {
+    hasil = 'Angka Tidak Valid!';
+  } else {
+    switch (operator) {
+      case '+': hasil = angka1 + angka2; break;
+      case '-': hasil = angka1 - angka2; break;
+      case '*': hasil = angka1 * angka2; break;
+      case '/': hasil = angka2 !== 0 ? (angka1 / angka2) : '0'; break;
+    }
+  }
+
+  document.getElementById('hasil').textContent = hasil;
+}
